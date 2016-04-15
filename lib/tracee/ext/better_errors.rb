3edbf698f71205cfbde8916c6bf52b29c4a712af
Sergey Baev <sbaev@at-consulting.ru>
2016-04-16 00:56:00 +0300

    BetterErrors: output exception trace lines referring to 3rd party to debug only

--- lib/tracee/ext/better_errors.rb
+++ lib/tracee/ext/better_errors.rb
@@ -9,18 +9,22 @@ module Tracee
         end
         
         def log_exception_with_decorate
-          return unless ::BetterErrors.logger
+          return unless logger = ::BetterErrors.logger
 
-          message = "\n#{@error_page.exception.class} - #{@error_page.exception.message}:\n"
+          message = "\n#{@error_page.exception.class} - #{@error_page.exception.message}:"
+
+          logger.fatal message
           
           frames = @error_page.backtrace_frames # original definition
           frames = frames.map(&:to_s).reject {|line| line =~ IGNORE_RE}
           frames = Stack::BaseDecorator.(frames)
           frames.each do |frame|
-            message << "  #{frame}\n"
+            if frame =~ /[-\w]+ \(\d+\.[\w\.]+\) /
+              logger.debug "  #{frame}"
+            else
+              logger.fatal "  #{frame}"
+            end
           end
-
-          ::BetterErrors.logger.fatal message
         end
         
       end
--- lib/tracee/version.rb
+++ lib/tracee/version.rb
@@ -1,3 +1,3 @@
 module Tracee
-  VERSION = "1.0.3"
+  VERSION = "1.0.4"
 end
