9e25a3e58ef1ee9b3212ec837bd88640c2cdf8bb
Sergey Baev <sbaev@at-consulting.ru>
2017-07-12 20:09:32 +0300

    Remove #alias_method_chain call

--- lib/tracee/ext/better_errors.rb
+++ lib/tracee/ext/better_errors.rb
@@ -5,24 +5,22 @@ module Tracee
         extend ::ActiveSupport::Concern
         
         included do
-          alias_method_chain :log_exception, :decorate
-        end
-        
-        def log_exception_with_decorate
-          return unless logger = ::BetterErrors.logger
+          def log_exception
+            return unless logger = ::BetterErrors.logger
 
-          message = "\n#{@error_page.exception.class} - #{@error_page.exception.message}:"
+            message = "\n#{@error_page.exception.class} - #{@error_page.exception.message}:"
 
-          logger.fatal message
-          
-          frames = @error_page.backtrace_frames # original definition
-          frames = frames.map(&:to_s).reject {|line| line =~ IGNORE_RE}
-          frames = Stack::BaseDecorator.(frames, paint_code_line: :greenish)
-          frames.each do |frame|
-            if frame =~ /^[-\w]+ \(\d+\.[\w\.]+\) /
-              logger.debug "  #{frame}"
-            else
-              logger.fatal "  #{frame}"
+            logger.fatal message
+            
+            frames = @error_page.backtrace_frames # original definition
+            frames = frames.map(&:to_s).reject {|line| line =~ IGNORE_RE}
+            frames = Stack::BaseDecorator.(frames, paint_code_line: :greenish)
+            frames.each do |frame|
+              if frame =~ /^[-\w]+ \(\d+\.[\w\.]+\) /
+                logger.debug "  #{frame}"
+              else
+                logger.fatal "  #{frame}"
+              end
             end
           end
         end
